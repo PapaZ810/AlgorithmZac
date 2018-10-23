ce47165b66d247bdb195c65a49ab0880615994be
Zac <zombiezac@att.net>
2018-10-23 12:40:38 -0600

    added string steps and called the formatAlgorithm method in view did load

--- AlgorithmZac/Controller/AlgorithmViewController.swift
+++ AlgorithmZac/Controller/AlgorithmViewController.swift
@@ -18,9 +18,19 @@ public class AlgorithmViewController: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        formatAlgorithm()
     }
-
     
+    private func formatAlgorithm() -> Void
+    {
+        let title : String = "How to Create an App in XCode"
+        
+        let stepOne : String = "Open XCode and click create XCode project."
+        let stepTwo : String = "Select Single View App and click next"
+        let stepThree : String = "Call you app whatever you want(no spaces) and add an organization name then click next."
+        let stepFour : String = "Save it to a new folder in your documents folder called iOSPlayground and deselect the create git repository on your mac option."
+        let stepFive : String = "Now you have to make your MVC and resources folders. select the appdelegate file, the assets.xcassets and the Info.plist file right click then select new group from selection. Call that folder Resources. Then select the 2 storyboard files and put them into a new group from selection and call it View. select the controller file and and put it into a new group from selection called Controller. Then create a new file call it model and put a new swift file called SomethingTool.swift."
+    }
 
 
 }
