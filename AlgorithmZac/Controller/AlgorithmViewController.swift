eccde43d49dc6edc8a20775bfbf5ac7a57a20ffb
Zac <zombiezac@att.net>
2018-10-23 12:29:13 -0600

    connected new storyboard elements: label and image

--- AlgorithmZac/Controller/AlgorithmViewController.swift
+++ AlgorithmZac/Controller/AlgorithmViewController.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class AlgorithmViewController: UIViewController
+{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var algorithmText: UILabel!
+    
+    public override func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
